4af20240760cf33019308ef5ebb83a939c2e99c0
MADAVAN K <kmadavan018@gmail.com>
2023-08-16 22:31:32 +0530

    Flask Structure

---
+++ views/views.py
@@ -0,0 +1,8 @@
+from flask import (Flask, flash, make_response, redirect, render_template,
+                   request, session, url_for)
+
+from app import *
+
+@app.route('/')
+def home():
+    return render_template('index.html')
