6a7a0d1b6729d02c18ee2bd665b8c2128e5878f4
MADAVAN K <kmadavan018@gmail.com>
2023-08-23 16:16:09 +0530

    bug fix

--- views/views.py
+++ views/views.py
@@ -5,7 +5,23 @@ import bcrypt
 
 from app import *
 
-# from modules.utils import *
+from modules.utils import *
+from modules.preprocessor import *
+
+common_language = {
+    "Bengali": "ben_Beng",
+    "Gujarati": "guj_Gujr",
+    "Hindi": "hin_Deva",
+    "Kannada": "kan_Knda",
+    "Malayalam": "mal_Mlym",
+    "Marathi": "mar_Deva",
+    "Nepali": "npi_Deva",
+    "Sinhala": "sin_Sinh",
+    "Tamil": "tam_Taml",
+    "Telugu": "tel_Telu",
+    "Urdu": "urd_Arab",
+    "English": "eng_Latn"
+}
 
 @app.route('/test')
 def test():
@@ -121,8 +137,8 @@ def text():
             text = request.form.get("text") 
             sourceLang = dectLang(text)
             translatedText = text2textTranslation(source=sourceLang, target=lag, text=text)
-            return render_template('texttotext.html', translatedText=translatedText,common_language = common_language)
-        return render_template('texttotext.html',common_language = common_language)
+            return render_template('text.html', translatedText=translatedText,common_language = common_language)
+        return render_template('text.html',common_language = common_language)
 
     else:
         return redirect('/login')
@@ -158,7 +174,7 @@ def video():
         #     lag= "tam_Taml"
         #     sourceLang = dectLang(text)
         #     translatedText = text2textTranslation(source=sourceLang, target=lag, text=text)
-        #     return render_template('texttotext.html', translatedText=translatedText)
+        #     return render_template('text.html', translatedText=translatedText)
         return render_template('videotovideo.html')
 
     else:
@@ -183,7 +199,7 @@ def audio():
                 translatedaudiofolder = os.path.join(app.config['UPLOAD_FOLDER'], 'translatedaudio')
                 translatedaudiopath = os.path.join(translatedaudiofolder, file.filename)
                 file.save(audiopath)
-                translated_text = englishaudio_englishtext(audiopath,language)
+                translated_text = englishaudio_englishtext(file.filename,language)
                 text_audio(translated_text,language,translatedaudiopath)
                 return translatedaudiopath
         #     # lag = request.form.get("lag")
@@ -191,7 +207,7 @@ def audio():
         #     lag= "tam_Taml"
         #     sourceLang = dectLang(text)
         #     translatedText = text2textTranslation(source=sourceLang, target=lag, text=text)
-        #     return render_template('texttotext.html', translatedText=translatedText)
+        #     return render_template('text.html', translatedText=translatedText)
         return render_template('videotovideo.html')
 
     else:
@@ -229,7 +245,7 @@ def youtubevideo():
         #     lag= "tam_Taml"
         #     sourceLang = dectLang(text)
         #     translatedText = text2textTranslation(source=sourceLang, target=lag, text=text)
-        #     return render_template('texttotext.html', translatedText=translatedText)
+        #     return render_template('text.html', translatedText=translatedText)
         return render_template('videotovideo.html')
 
     else:
